5d487575bf40b892f9e5070db65575ddf9985ec2
Blessing Agbali <blessagbali20@gmail.com>
2022-12-19 15:54:29 +0100

    Edited footer and deleted some unnecessary files

--- src/Footer.js
+++ src/Footer.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 export default function Footer() {
-    return (
-      <footer className="github">
-        <a href="https://github.com/Agbali/React-Weather-App">
-          My Weather App Github Repository
-        </a>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="github">
+      This project was created by{" "}
+      <a href="https://twitter.com/omoaloEse" rel="noreferrer" target="_blank">
+        Omoalo
+      </a>{" "}
+      and it is{" "}
+      <a
+        href="https://github.com/Agbali/React-Weather-App"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Open-sourced on Github
+      </a>
+    </footer>
+  );
+}
